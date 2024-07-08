bf4d3ef5cf286170ab7feb31bd8d10af06889127
türkay ürkmez <turkay.urkmez@outlook.com>
2024-07-08 11:11:42 +0300

    initialize repo

---
+++ InjectionAttacks/InjectionAttacks/Models/UserLoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InjectionAttacks.Models
+{
+    public class UserLoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
